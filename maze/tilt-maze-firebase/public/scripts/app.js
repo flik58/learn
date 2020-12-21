b270fc2e2856afe844358fa0542c540c77396e85
flik58 <craft.sh58@gmail.com>
2020-12-22 00:02:56 +0900

    fix: split path

---
+++ maze/tilt-maze-firebase/public/scripts/app.js
@@ -0,0 +1,6 @@
+import {maze} from "./maze.js";
+import * as auth from "./auth.js";
+
+auth.initFirebaseAuth();
+maze();
+
---
+++ maze/tilt-maze-firebase/public/scripts/config.js
@@ -0,0 +1,76 @@
+import {wallW, ballSize} from "./items.js"
+
+export const distance2D = (p1, p2) => {
+  return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
+};
+
+// Angle between the two points
+export const getAngle = (p1, p2) => {
+  let angle = Math.atan((p2.y - p1.y) / (p2.x - p1.x));
+  if (p2.x - p1.x < 0) angle += Math.PI;
+  return angle;
+};
+
+// The closest a ball and a wall cap can be
+export const closestItCanBe = (cap, ball) => {
+  let angle = getAngle(cap, ball);
+
+  const deltaX = Math.cos(angle) * (wallW / 2 + ballSize / 2);
+  const deltaY = Math.sin(angle) * (wallW / 2 + ballSize / 2);
+
+  return { x: cap.x + deltaX, y: cap.y + deltaY };
+};
+
+// Roll the ball around the wall cap
+export const rollAroundCap = (cap, ball) => {
+  // The direction the ball can't move any further because the wall holds it back
+  let impactAngle = getAngle(ball, cap);
+
+  // The direction the ball wants to move based on it's velocity
+  let heading = getAngle(
+    { x: 0, y: 0 },
+    { x: ball.velocityX, y: ball.velocityY }
+  );
+
+  // The angle between the impact direction and the ball's desired direction
+  // The smaller this angle is, the bigger the impact
+  // The closer it is to 90 degrees the smoother it gets (at 90 there would be no collision)
+  let impactHeadingAngle = impactAngle - heading;
+
+  // Velocity distance if not hit would have occurred
+  const velocityMagnitude = distance2D(
+    { x: 0, y: 0 },
+    { x: ball.velocityX, y: ball.velocityY }
+  );
+  // Velocity component diagonal to the impact
+  const velocityMagnitudeDiagonalToTheImpact =
+        Math.sin(impactHeadingAngle) * velocityMagnitude;
+
+  // How far should the ball be from the wall cap
+  const closestDistance = wallW / 2 + ballSize / 2;
+
+  const rotationAngle = Math.atan(
+    velocityMagnitudeDiagonalToTheImpact / closestDistance
+  );
+
+  const deltaFromCap = {
+    x: Math.cos(impactAngle + Math.PI - rotationAngle) * closestDistance,
+    y: Math.sin(impactAngle + Math.PI - rotationAngle) * closestDistance
+  };
+
+  const x = ball.x;
+  const y = ball.y;
+  const velocityX = ball.x - (cap.x + deltaFromCap.x);
+  const velocityY = ball.y - (cap.y + deltaFromCap.y);
+  const nextX = x + velocityX;
+  const nextY = y + velocityY;
+
+  return { x, y, velocityX, velocityY, nextX, nextY };
+};
+
+// Decreases the absolute value of a number but keeps it's sign, doesn't go below abs 0
+export const slow = (number, difference) => {
+  if (Math.abs(number) <= difference) return 0;
+  if (number > difference) return number - difference;
+  return number + difference;
+};
